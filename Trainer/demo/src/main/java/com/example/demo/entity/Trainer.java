3f3b9a6c3a32a48e355eee49a76cd31ef3f6db81
dhia-trabelsi <trabelsidhia7@gmail.com>
2022-12-31 02:54:40 +0100

    trainer

---
+++ Trainer/demo/src/main/java/com/example/demo/entity/Trainer.java
@@ -0,0 +1,38 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "trainer")
+public class Trainer {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer id;
+
+	public String name;
+	@Column(length = 2000, columnDefinition = "TEXT")
+	public String mail;
+	public Date date;
+	private String imageUrl;
+	private String password;
+    
+}
